edb18af09ea92e0209f56a6aab34ec852cc5753b
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-06-07 17:31:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const ul=document.getElementByTagName("ul")[0];
 let displayText;
 for(let i=0;i<ul.children.length;i++){
 	let node=ul.children[i];
-	if(node#id=="level"){
+	if(node.id=="level"){
 		displayText="The level of the element is: "+(i+1);
 	}
 }
