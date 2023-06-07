68d693daf0a0ad55e023e0b24fa4fed460a8e43d
Mamta <120008345+Mamta13kunal@users.noreply.github.com>
2023-06-07 17:55:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //your JS code here. If required.
-const ul=document.getElementByTagName("ul")[0];
+const ul=document.getElementsByTagName("ul")[0];
 let displayText;
-for(let i=0;i<ul.children.length;i++){
+
+for(let i=0; i<ul.children.length;i++){
 	let node=ul.children[i];
 	if(node.id=="level"){
 		displayText="The level of the element is: "+(i+1);
